336187287ef9529084477744c5ca8694303f5693
Liusvani <liusvani@gmail.com>
2020-01-14 13:35:57 -0500

    cambios del dist

---
+++ dist/browser/assets/js/adnow.js
@@ -0,0 +1,10 @@
+
+    (sc_adv_out = window.sc_adv_out || []).push({
+		id : "716616",
+		domain : "n.ads1-adnow.com"
+	});
+
+	(sc_adv_out = window.sc_adv_out || []).push({
+		id : "716617",
+		domain : "n.ads1-adnow.com"
+	});
