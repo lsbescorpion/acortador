031dfc16c266b1170fd54a420e75ae4da6b44c15
Liusvani <liusvani@gmail.com>
2020-02-14 07:51:59 -0500

    otros adsense

--- dist/server/main.js
+++ dist/server/main.js
@@ -7357,10 +7357,10 @@ var TemporalComponent = /** @class */ (function () {
                 var scriptnow = document.createElement('script');
                 scriptnow.innerHTML = '(adsbygoogle = window.adsbygoogle || []).push({});';
                 //scriptnow.src = this.globals.globalUrl + "/assets/js/adsense.js";
-                var bloque1 = document.getElementById('#bloque1');
+                var bloque1 = _this.document.getElementById('bloque1');
                 bloque1.insertBefore(script, bloque1.childNodes[0]);
                 bloque1.appendChild(scriptnow);
-                var bloque2 = document.getElementById('#bloque1');
+                var bloque2 = _this.document.getElementById('bloque2');
                 bloque2.insertBefore(script, bloque1.childNodes[0]);
                 bloque2.appendChild(scriptnow);
                 var da = data;
