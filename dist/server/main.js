84f99e1fd5c7e69fed7f65a9366c578d6c972fee
Liusvani <liusvani@gmail.com>
2020-02-14 13:03:50 -0500

    otros adsense

--- dist/server/main.js
+++ dist/server/main.js
@@ -7269,6 +7269,41 @@ exports.styles = styles;
 
 "use strict";
 
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = __webpack_require__(/*! @angular/core */ "@angular/core");
 var router_1 = __webpack_require__(/*! @angular/router */ "@angular/router");
@@ -7352,25 +7387,6 @@ var TemporalComponent = /** @class */ (function () {
                 scriptnow.type = "text/javascript";
                 scriptnow.src = "//st-n.ads1-adnow.com/js/a.js";
                 document.body.appendChild(scriptnow);*/
-                var script = document.createElement('script');
-                script.async = true;
-                script.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js";
-                var scriptnow = document.createElement('script');
-                scriptnow.innerHTML = '(adsbygoogle = window.adsbygoogle || []).push({});';
-                var script1 = document.createElement('script');
-                script1.async = true;
-                script1.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js";
-                var scriptnow1 = document.createElement('script');
-                scriptnow1.innerHTML = '(adsbygoogle = window.adsbygoogle || []).push({});';
-                //scriptnow.src = this.globals.globalUrl + "/assets/js/adsense.js";
-                setTimeout(function () {
-                    var bloque1 = document.getElementById('bloque1');
-                    bloque1.insertBefore(script, bloque1.childNodes[0]);
-                    bloque1.appendChild(scriptnow);
-                    var bloque2 = document.getElementById('bloque2');
-                    bloque2.insertBefore(script1, bloque2.childNodes[0]);
-                    bloque2.appendChild(scriptnow1);
-                }, 0);
                 var da = data;
                 _this.url = da;
                 var link = document.createElement('link');
@@ -7396,6 +7412,7 @@ var TemporalComponent = /** @class */ (function () {
                 _this.title = da.titulo;
                 var that = _this;
                 var fiveSeconds = new Date().getTime() + 20000;
+                _this.setScript();
                 setTimeout(function () {
                     $('#clock').countdown(fiveSeconds, function (event) {
                         $(this).html("Por favor espere: " + event.strftime('%S') + " Generando enlace");
@@ -7435,6 +7452,38 @@ var TemporalComponent = /** @class */ (function () {
                 }
             });
         }
+    };
+    TemporalComponent.prototype.setScript = function () {
+        return __awaiter(this, void 0, void 0, function () {
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0: return [4 /*yield*/, this.Esperar()];
+                    case 1:
+                        _a.sent();
+                        return [2 /*return*/];
+                }
+            });
+        });
+    };
+    TemporalComponent.prototype.Esperar = function () {
+        var script = document.createElement('script');
+        //script.async = true;
+        script.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js";
+        var scriptnow = document.createElement('script');
+        scriptnow.innerHTML = '(adsbygoogle = window.adsbygoogle || []).push({});';
+        var script1 = document.createElement('script');
+        //script1.async = true;
+        script1.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js";
+        var scriptnow1 = document.createElement('script');
+        scriptnow1.innerHTML = '(adsbygoogle = window.adsbygoogle || []).push({});';
+        setTimeout(function () {
+            var bloque1 = document.getElementById('bloque1');
+            bloque1.insertBefore(script, bloque1.childNodes[0]);
+            bloque1.appendChild(scriptnow);
+            var bloque2 = document.getElementById('bloque2');
+            bloque2.insertBefore(script1, bloque2.childNodes[0]);
+            bloque2.appendChild(scriptnow1);
+        }, 0);
     };
     TemporalComponent.prototype.onFinished = function () {
         /*var link = '<a href="'+this.url.url_real+'" style="color: #FFFFFF;">Acceder al artículo completo aquí</a>';
