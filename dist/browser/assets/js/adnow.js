a653ec761079502fc63c4da83f27aa7e8f4d6b24
Liusvani <liusvani@gmail.com>
2020-09-30 11:47:20 -0400

    cambios navbar

---
+++ dist/browser/assets/js/adnow.js
@@ -0,0 +1,6 @@
+$( document ).ready(function() {
+    (sc_adv_out = window.sc_adv_out || []).push({
+	    id : "721506",
+	    domain : "n.ads1-adnow.com"
+	});
+});
---
+++ dist/browser/assets/js/adsense.js
@@ -0,0 +1,3 @@
+$( document ).ready(function() {
+    (adsbygoogle = window.adsbygoogle || []).push({});
+});
